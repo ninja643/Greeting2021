c962960ebc306b08c46ec62f35840d5f17386728
Marko Milosevic <markom@niri.rs>
2021-02-23 14:13:02 +0100

    Initial commit

---
+++ app/src/main/java/rs/ac/ni/pmf/android/greeting/GreetingActivity.java
@@ -0,0 +1,16 @@
+package rs.ac.ni.pmf.android.greeting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GreetingActivity extends AppCompatActivity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.greeting_activity);
+	}
+}
